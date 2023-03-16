85b5a6d30e23f062d680472a7f4be1c096908ea4
riyo3350g <yechouben@gmail.com>
2023-03-16 07:46:09 -0700

    6 tasks added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
-#include<stdio.h>
-/*
- * comment
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
