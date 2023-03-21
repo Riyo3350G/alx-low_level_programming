0eb497cccbd34a4fe55af1c35541d2223b3a5809
riyo3350g <yechouben@gmail.com>
2023-03-21 10:56:20 -0700

    Alphabet added

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,11 @@
  */
 void print_alphabet(void)
 {
-    int a;
+	int a;
 
-    for (a = 97; a <= 122; a++)
-    {
-        _putchar(a);
-    }
-    _putchar('\n');
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
 }
