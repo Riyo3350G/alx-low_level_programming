905097462ec3ee35ea18dd887d9a7ae98d90020c
riyo3350g <yechouben@gmail.com>
2023-03-21 08:25:39 -0700

    0-putchar.c added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,25 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - Prints _putchar and new line.
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
-    printf("_putchar\n");
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
     return (0);
+}
+
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
 }
