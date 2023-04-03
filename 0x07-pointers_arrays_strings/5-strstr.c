5c444ceb092fd8e118846d05415e6e05c01203be
riyo3350g <yechouben@gmail.com>
2023-04-03 07:06:24 -0700

    5-strstr.c added

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strstr - Locates a substring
+ * @haystack: String to look though
+ * @needle: Substring to find
+ * Return: Pointer to string with result of search
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int length;
+	/**/
+	length = 0;
+	while (needle[length] != '\0')
+		length++;
+	/**/
+	while (*haystack != '\0')
+	{
+		if (_strncmp(haystack, needle, length) == 0)
+			return (haystack);
+		haystack++;
+	}
+	return (NULL);
+}
