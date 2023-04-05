583d582e3fbc29d9d116affc36fde895121692a3
riyo3350g <yechouben@gmail.com>
2023-04-05 06:32:30 -0700

    update 5-sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,20 +8,20 @@
  */
 int _sqrt_recursion(int n)
 {
-	return (sqrt(n, 1));
+	return (_sqrt(n, 1));
 }
 
 /**
- * sqrt - Makes possible to evaluate from 1 to n
+ * _sqrt - Makes possible to evaluate from 1 to n
  * @a: same number as n
  * @b: number that iterates from 1 to n
  *
  */
-int sqrt(int a, int b)
+int _sqrt(int a, int b)
 {
         if (b * b == a)
                 return (b);
         else if (b * b > a)
                 return (-1);
-        return (sqrt(a, b + 1));
+        return (_sqrt(a, b + 1));
 }
