af3971de34d0dcfefc0ec1692110ba29fbd0d24d
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-04-13 15:51:27 -0700

    Try some fixes

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,30 +4,30 @@
 #include <ctype.h>
 
 /**
- * _is_zero - Function to determines if any number is zero
- * @argv: The argument vector.
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
  *
  * Return: no return.
  */
 void _is_zero(char *argv[])
 {
-	int i, is_zero1 = 1, is_zero2 = 1;
+	int i, isn1 = 1, isn2 = 1;
 
 	for (i = 0; argv[1][i]; i++)
 		if (argv[1][i] != '0')
 		{
-			is_zero1 = 0;
+			isn1 = 0;
 			break;
 		}
 
 	for (i = 0; argv[2][i]; i++)
 		if (argv[2][i] != '0')
 		{
-			is_zero2 = 0;
+			isn2 = 0;
 			break;
 		}
 
-	if (is_zero1 == 1 || is_zero2 == 1)
+	if (isn1 == 1 || isn2 == 1)
 	{
 		printf("0\n");
 		exit(0);
@@ -35,49 +35,49 @@ void _is_zero(char *argv[])
 }
 
 /**
- * _initialize_array - Function to set memery to zero in a new array
- * @ar: The char array.
- * @lar: The length of the char array.
+ * _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
  *
- * Return: The pointer of a char array.
+ * Return: pointer of a char array.
  */
 char *_initialize_array(char *ar, int lar)
 {
-	int a = 0;
+	int i = 0;
 
-	for (a = 0; a < lar; a++)
-		ar[a] = '0';
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
 	ar[lar] = '\0';
 	return (ar);
 }
 
 /**
- * _checknum - Function to determines length of the number
+ * _checknum - determines length of the number
  * and checks if number is in base 10.
- * @argv: The arguments vector.
- * @n: The row of the array.
+ * @argv: arguments vector.
+ * @n: row of the array.
  *
- * Return: The length of the number.
+ * Return: length of the number.
  */
 int _checknum(char *argv[], int n)
 {
-	int len;
+	int ln;
 
-	for (len = 0; argv[n][len]; len++)
-		if (!isdigit(argv[n][len]))
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
 		{
 			printf("Error\n");
 			exit(98);
 		}
 
-	return (len);
+	return (ln);
 }
 
 /**
  * main - Entry point.
  * program that multiplies two positive numbers.
- * @argc: The number of arguments.
- * @argv: The arguments vector.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
  *
  * Return: 0 - success.
  */
