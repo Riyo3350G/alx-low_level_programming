ec2f7782b2fd31777fd0774f608e51f42bb3a5e6
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-04-17 18:16:23 -0700

    Update 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,46 +1,54 @@
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * new_dog - creates a new dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
  *
- * Return: a pointer to the new dog 
- * or NULL if memory allocation fails
+ * Return: struct dog.
+ * if fails, returns NULL.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
-    char *new_name, *new_owner;
+	dog_t *p;
+	int i; 
+    int d_name, d_owner;
 
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
+	p = malloc(sizeof(*p));
+	if (p == NULL || !(name) || !(owner))
+	{
+		free(p);
+		return (NULL);
+	}
 
-    new_name = malloc(sizeof(char) * (strlen(name) + 1));
-    if (new_name == NULL)
-    {
-        free(new_dog);
-        return (NULL);
-    }
-    strcpy(new_name, name);
+	for (d_name = 0; name[d_name]; d_name++)
+		;
 
-    new_owner = malloc(sizeof(char) * (strlen(owner) + 1));
-    if (new_owner == NULL)
-    {
-        free(new_name);
-        free(new_dog);
-        return (NULL);
-    }
-    strcpy(new_owner, owner);
+	for (d_owner = 0; owner[d_owner]; d_owner++)
+		;
 
-    new_dog->name = new_name;
-    new_dog->age = age;
-    new_dog->owner = new_owner;
+	p->name = malloc(d_name + 1);
+	p->owner = malloc(d_owner + 1);
 
-    return (new_dog);
+	if (!(p->name) || !(p->owner))
+	{
+		free(p->owner);
+		free(p->name);
+		free(p);
+		return (NULL);
+	}
+
+	for (i = 0; i < d_name; i++)
+		p->name[i] = name[i];
+	p->name[i] = '\0';
+
+	p->age = age;
+
+	for (i = 0; i < d_owner; i++)
+		p->owner[i] = owner[i];
+	p->owner[i] = '\0';
+
+	return (p);
 }
-
