7cabcd4f06f10e9bbfe5e16917ec4d11e267ae6e
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-04-17 17:10:14 -0700

    Update

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 
 /**
  * init_dog - initializes a variable of type struct dog.
- * @d: struct dog.
+ * @d: pointer to struct dog.
  * @name: name of the dog.
  * @age: age of the dog.
  * @owner: owner of the dog.
@@ -11,10 +11,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d)
 	{
 		d->name = name;
-		d->age = age;
 		d->owner = owner;
+		d->age = age;
 	}
 }
