3c71f5a5e18552e182afd0f283a56dcc56394f5d
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-04-19 10:22:16 -0700

    Update some fixes

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,12 +10,9 @@
  * */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int a;
+	size_t a;
+	
 	if(array && action)
-	{
 		for(a = 0; a < size; a++)
-		{
 			action(array[a]);
-		}
-	}
 }
