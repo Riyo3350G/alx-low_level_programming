66efac6ff91322695c8847589983ed0d0835159e
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-04-20 05:29:00 -0700

    Update 3-get_op_func.c betty fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,33 +3,32 @@
 #include <stdio.h>
 
 /**
- * get_op_func - To select the correct function to do
- * the operation asked by the user.
- * @s: Operator type char.
+ * get_op_func - Selects the correct function to perform
+ *               the operation requested by the user.
+ * @s: Operator as a string.
  *
- * Return: Pointer to the function corresponding
- * to the operator given as a parameter.
+ * Return: Pointer to the corresponding function, or NULL
+ *         if the operator is invalid.
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
+        op_t ops[] = {
+                {"+", op_add},
+                {"-", op_sub},
+                {"*", op_mul},
+                {"/", op_div},
+                {"%", op_mod},
+                {NULL, NULL}
+        };
 
-	int a;
-	a = 0;
-	
-	while (ops[a].op != NULL && s[1] == '\0')
-	{
-		if (ops[a].op[0] == *s)
-			return ((ops[a].f));
-		a++;
-	}
+        int a = 0;
 
-	return (NULL);
+        while (ops[a].op != NULL && s[1] == '\0')
+        {
+                if (ops[a].op[0] == *s)
+                        return (ops[a].f);
+                a++;
+        }
+
+        return (NULL);
 }
