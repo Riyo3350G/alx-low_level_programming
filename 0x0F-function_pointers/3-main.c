94d8e0e5af24b4b43c6c2411ea3686afe94a4f32
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-04-19 18:34:56 -0700

    Update 3-main,c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,32 +14,23 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2;
-    char *op;
+        int num1, num2;
+        char *op;
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+        if (argc != 4)
+        {
+                printf("Error\n");
+                exit(98);
+        }
+        num1 = atoi(argv[1]);
+        num2 = atoi(argv[3]);
+        op = argv[2];
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    op = argv[2];
+        (ARGV(2, '/') || ARGV(2, '%')) && num2 == 0 ? 
+            printf("Error\n"), exit(100) : 0;
 
-    if ((ARGV(2, '/') || ARGV(2, '%')) && num2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+        !(get_op_func(op)) ? printf("Error\n"), exit(99) : 0;
 
-    if (!(get_op_func(op)))
-    {
-        printf("Error\n");
-        exit(99);
-    }
-
-    printf("%d\n", get_op_func(op)(num1, num2));
-
-    return (0);
+        printf("%d\n", get_op_func(op)(num1, num2));
+        return (0);
 }
