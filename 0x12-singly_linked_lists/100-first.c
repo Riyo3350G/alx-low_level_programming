6a690ec40f82d1998c443d1a340a0dcd1fc28a13
riyo3350g <yechouben@gmail.com>
2023-04-27 12:50:26 -0700

    advanced 1 added

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * first - Prints a message before main function.
+ *
+ * Return: no return
+ */
+void __attribute__ ((constructor)) first()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
