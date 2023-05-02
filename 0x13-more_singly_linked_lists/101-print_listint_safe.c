a87daf809f5a7449e4342b7d413fd5842840aea6
riyo3350g <yechouben@gmail.com>
2023-05-02 06:31:03 -0700

    task 12 added

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - Prints a linked list in safe mode
+ * @head: Pointer to the beginning of the linked list
+ *
+ * Return: The number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *turtle = head, *rabbit = head;
+	size_t ctr = 0;
+
+	while (turtle != NULL && rabbit != NULL && rabbit->next != NULL)
+	{
+		turtle = turtle->next;
+		rabbit = rabbit->next->next;
+
+		if (turtle == rabbit)
+		{
+			printf("[%p] %d\n", (void *)turtle, turtle->n);
+			ctr++;
+			while (turtle != rabbit->next)
+			{
+				printf("[%p] %d\n", (void *)turtle, turtle->n);
+				ctr++;
+				turtle = turtle->next;
+			}
+			return (ctr);
+		}
+
+		printf("[%p] %d\n", (void *)turtle, turtle->n);
+		ctr++;
+	}
+
+	return (ctr);
+}
