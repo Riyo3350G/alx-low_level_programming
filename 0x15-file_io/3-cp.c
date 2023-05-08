fc6cccb7718b78e5a99007032ea06ca4f2b0ac6a
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-05-08 16:17:35 -0700

    Update 3-cp.c with betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,8 +18,10 @@ int main(int argc, char *argv[])
 
 	op_from = open(argv[1], O_RDONLY);
 	if (op_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	op_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (op_dest == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
@@ -28,7 +30,10 @@ int main(int argc, char *argv[])
 	{
 		wr = write(op_dest, buff, rd);
 		if (wr != rd || wr == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
 
 	if (rd == -1)
