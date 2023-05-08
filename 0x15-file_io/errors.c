1ea5aedd61d11bba927a35134f45d30db9ff9b86
riyo3350g <yechouben@gmail.com>
2023-05-08 15:36:08 -0700

    errors betty fixeded

--- 0x15-file_io/errors.c
+++ 0x15-file_io/errors.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
-
+/**
+ * errors - Prints an error message and exits with a given status
+ *
+ * @statu: The status to exit with.
+ * @message: The error message to be printed.
+ */
 void errors(int statu, const char *message)
 {
 	dprintf(STDERR_FILENO, "%s\n", message);
