561bd69cc0997a931c5e449da0869bce8552f4f1
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-10-10 18:13:52 +0100

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,6 +13,4 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 
-/* Helper functions prototype*/
-int _binary_search(int *array, size_t left, size_t right, int value);
 #endif /* _SEARCH_ALGOS_H_ */
